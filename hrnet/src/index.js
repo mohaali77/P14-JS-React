a05e24af299a3752bf57afae2cddf905e5b1652a
mohaali77 <mzr.mohand@gmail.com>
2023-12-20 22:53:52 +0100

    Update index.js

--- hrnet/src/index.js
+++ hrnet/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App'
 import './App.css'
-import Header from './components/Header';
+import Header from './components/header';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
