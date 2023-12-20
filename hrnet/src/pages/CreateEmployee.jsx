e48a0c97f918236e665e310956b89def5c8b0add
mohaali77 <mzr.mohand@gmail.com>
2023-12-20 11:41:02 +0100

    Update CreateEmployee.jsx

--- hrnet/src/pages/CreateEmployee.jsx
+++ hrnet/src/pages/CreateEmployee.jsx
@@ -1,6 +1,42 @@
+import Menu from "../components/Menu";
+
 export function CreateEmployee() {
     return <>
-        <div>
-        </div>
+        <section className="container">
+            <a href="employee-list.html">View Current Employees</a>
+            <h2>Create Employee</h2>
+            <form action="#" id="create-employee">
+                <label for="first-name">First Name</label>
+                <input type="text" id="first-name" />
+
+                <label for="last-name">Last Name</label>
+                <input type="text" id="last-name" />
+
+                <label for="date-of-birth">Date of Birth</label>
+                <input id="date-of-birth" type="text" />
+
+                <label for="start-date">Start Date</label>
+                <input id="start-date" type="text" />
+
+                <fieldset class="address">
+                    <legend>Address</legend>
+
+                    <label for="street">Street</label>
+                    <input id="street" type="text" />
+
+                    <label for="city">City</label>
+                    <input id="city" type="text" />
+
+                    <label for="state">State</label>
+                    <select name="state" id="state"></select>
+
+                    <label for="zip-code">Zip Code</label>
+                    <input id="zip-code" type="number" />
+                </fieldset>
+
+                <Menu />
+            </form>
+        </section>
     </>
 }
+
