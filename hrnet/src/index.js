f7e93b96ce329a2f0c801a3b3c6dad351b86f260
mohaali77 <mzr.mohand@gmail.com>
2023-12-20 23:09:57 +0100

    Update index.js

--- hrnet/src/index.js
+++ hrnet/src/index.js
@@ -4,12 +4,13 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App'
 import './App.css'
 import Header from './components/header';
+import Background from './components/background';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Header />
+      <Background />
       <App />
     </BrowserRouter>
   </React.StrictMode>
