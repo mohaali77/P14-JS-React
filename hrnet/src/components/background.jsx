4e6d5fedd57ac09ec3e8495ffea4576e1e93c5ca
mohaali77 <mzr.mohand@gmail.com>
2023-12-20 22:59:21 +0100

    Create background.jsx

---
+++ hrnet/src/components/background.jsx
@@ -0,0 +1,10 @@
+import background from '../images/background.png'
+import './header.css'
+
+export default function Header() {
+    return <>
+        <div>
+            <img src={background} alt="" />
+        </div>
+    </>
+}
