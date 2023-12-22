ef1d3f8e3c0643d7418a1b2bfbd2b251333bfe8d
mohaali77 <mzr.mohand@gmail.com>
2023-12-22 10:37:55 +0100

    Update EmployeeList.jsx

--- hrnet/src/pages/EmployeeList/EmployeeList.jsx
+++ hrnet/src/pages/EmployeeList/EmployeeList.jsx
@@ -2,11 +2,9 @@ import { Link } from "react-router-dom";
 import './style/EmployeeList.css'
 
 export function EmployeeList() {
+
     return <>
-        <div id="employee-div" class="container">
-            <h2>Current Employees</h2>
-            <table id="employee-table" class="display"></table>
-            <Link to="/">Home</Link>
-        </div>
+        <h2>Current Employees</h2>
+        <Link to="/">Home</Link>
     </>
 }
