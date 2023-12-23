6994a3d95d8cbc553e44c3c886412dd359268860
mohaali77 <mzr.mohand@gmail.com>
2023-12-23 01:08:38 +0100

    Update date-picker.jsx

--- hrnet/src/components/date-picker/date-picker.jsx
+++ hrnet/src/components/date-picker/date-picker.jsx
@@ -1,11 +1,19 @@
 import './style/date-picker.css'
 
-export function DatePicker({ id, labelText, ref }) {
+export function DatePicker({ id, labelText, inputRef, onInputChange }) {
+
+    const handleChange = () => {
+        // Accéder à la valeur avec inputRef.current.value
+        const newValue = inputRef.current.value;
+        // Appeler la fonction du parent avec la nouvelle valeur
+        onInputChange(newValue);
+    };
+
     return <>
         <label id={id} htmlFor={id}>
             {labelText}
         </label>
-        <input ref={ref} type="date" id={id} placeholder="A" />
+        <input required ref={inputRef} onChange={handleChange} type="date" id={id} placeholder="A" />
     </>
 }
 
