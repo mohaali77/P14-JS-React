37d1941e01401c9809b30ec39ad474c4ea1d4215
mohaali77 <mzr.mohand@gmail.com>
2023-12-23 01:08:45 +0100

    Update menu.jsx

--- hrnet/src/components/menu/menu.jsx
+++ hrnet/src/components/menu/menu.jsx
@@ -1,13 +1,19 @@
 import './style/menu.css'
 
+export default function Menu({ className, name, id, forName, text, data, inputRef, onInputChange }) {
 
-export default function Menu({ className, name, id, forName, text, data, ref }) {
+    const handleChange = () => {
+        // Accéder à la valeur avec inputRef.current.value
+        const newValue = inputRef.current.value;
+        // Appeler la fonction du parent avec la nouvelle valeur
+        onInputChange(newValue);
+    };
 
     return <>
         <label className={className} htmlFor={forName}>{text}</label>
-        <select ref={ref} name={name} id={id}>
+        <select required ref={inputRef} onChange={handleChange} name={name} id={id}>
             {data.map((data) => (
-                <option key={data.abbreviation} value={data.abbreviation}>
+                <option key={data.abbreviation} value={data.name}>
                     {data.name}
                 </option>
             ))}
