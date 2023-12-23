dfa0c1bba9df3c925933d30702461de581f7bc76
mohaali77 <mzr.mohand@gmail.com>
2023-12-23 16:39:14 +0100

    Update form.jsx

--- hrnet/src/components/form/form.jsx
+++ hrnet/src/components/form/form.jsx
@@ -16,39 +16,41 @@ export function Form() {
     const inputZipCode = useRef('');
     const inputDepartment = useRef('');
 
-    const [errMsgFirst, setDataFirst] = useState('');
-    const [errMsgLast, seterrMsgLast] = useState('');
-    const [errMsgBirth, seterrMsgBirth] = useState('');
-    const [errMsgStart, seterrMsgStart] = useState('');
-    const [errMsgStreet, seterrMsgStreet] = useState('');
-    const [errMsgCity, seterrMsgCity] = useState('');
-    const [errMsgState, seterrMsgState] = useState('');
-    const [errMsgZipCode, seterrMsgZipCode] = useState('');
-    const [errMsgDepartment, seterrMsgDepartment] = useState('');
+    const [form, setForm] = useState({});
+
+    const errMsgFirst = useRef(false);
+    const errMsgLast = useRef(false);
+    const errMsgBirth = useRef(false);
+    const errMsgStart = useRef(false);
+    const errMsgStreet = useRef(false);
+    const errMsgCity = useRef(false);
+    const errMsgState = useRef(false);
+    const errMsgZipCode = useRef(false);
+    const errMsgDepartment = useRef(false);
 
     const [dataStates, setDataStates] = useState(statesArray);
     const [dataDepartments, setDataDepartments] = useState(departmentsArray);
 
-    const handleInputChange = () => {
-        console.log(
-            `Date Naissance : ${inputDateBirth.current.value}`,
-            `Date Début : ${inputDateStart.current.value}`,
-            `Departement : ${inputDepartment.current.value}`);
-    };
+    const handleInputChange = () => { }
+
+    const showErrorMsg = () => {
+
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        let form = {
-            firstname: inputFirstName.current.value,
-            lastname: inputLastName.current.value,
-            dateBirth: inputDateBirth.current.value,
-            dateStart: inputDateStart.current.value,
-            street: inputStreet.current.value,
-            city: inputCity.current.value,
-            state: inputState.current.value,
-            zipCode: inputZipCode.current.value,
-            department: inputDepartment.current.value,
-        }
+        setForm(
+            {
+                firstname: inputFirstName.current.value,
+                lastname: inputLastName.current.value,
+                dateBirth: inputDateBirth.current.value,
+                dateStart: inputDateStart.current.value,
+                street: inputStreet.current.value,
+                city: inputCity.current.value,
+                state: inputState.current.value,
+                zipCode: inputZipCode.current.value,
+                department: inputDepartment.current.value,
+            })
         console.log(form);
     }
 
@@ -61,17 +63,18 @@ export function Form() {
 
                 <label htmlFor="first-name">First Name</label>
                 <input required ref={inputFirstName} type="text" id="first-name" />
-                <p className='errorMsg'>Prénom invalide</p>
+                {errMsgFirst ? <p className='errorMsg'>Prénom invalide</p> : null}
+
 
                 <label htmlFor="last-name">Last Name</label>
                 <input required ref={inputLastName} type="text" id="last-name" />
-                <p className='errorMsg'>Nom invalide</p>
+                {errMsgLast ? <p className='errorMsg'>Nom invalide</p> : null}
 
                 <DatePicker inputRef={inputDateBirth} onInputChange={handleInputChange} labelText='Date of Birth' id='date-of-birth' />
-                <p className='errorMsg'>Date de naissance invalide</p>
+                {errMsgBirth ? <p className='errorMsg'>Date de naissance invalide</p> : null}
 
                 <DatePicker inputRef={inputDateStart} onInputChange={handleInputChange} labelText='Start Date' id='start-date' />
-                <p className='errorMsg'>Date de début invalide</p>
+                {errMsgStart ? <p className='errorMsg'>Date de début invalide</p> : null}
             </fieldset>
 
 
@@ -80,17 +83,17 @@ export function Form() {
 
                 <label htmlFor="street">Street</label>
                 <input required ref={inputStreet} id="street" type="text" />
-                <p className='errorMsg'>Date de début invalide</p>
+                {errMsgFirst ? <p className='errorMsg'>Prénom invalide</p> : null}
 
                 <label htmlFor="city">City</label>
                 <input required ref={inputCity} id="city" type="text" />
-                <p className='errorMsg'>Date de début invalide</p>
+                {errMsgFirst ? <p className='errorMsg'>Prénom invalide</p> : null}
 
                 <Menu inputRef={inputState} onInputChange={handleInputChange} data={dataStates} text='State' className='label-state' htmlFor='state' name='state' id="state" />
 
                 <label htmlFor="zip-code">Zip Code</label>
                 <input required ref={inputZipCode} id="zip-code" type="number" />
-                <p className='errorMsg'>Date de début invalide</p>
+                {errMsgFirst ? <p className='errorMsg'>Prénom invalide</p> : null}
 
             </fieldset>
             <Menu inputRef={inputDepartment} onInputChange={handleInputChange} data={dataDepartments} text='Department' className='label-department' htmlFor='department' name='department' id="department" />
