ca6a0dc7731cb706c814a91af57465e26453c67b
mohaali77 <mzr.mohand@gmail.com>
2023-12-24 11:16:01 +0100

    Update index.js

--- hrnet/src/index.js
+++ hrnet/src/index.js
@@ -6,16 +6,19 @@ import './App.css'
 import Header from './components/header/header';
 import Background from './components/background/background';
 import { Provider } from 'react-redux';
-import store from "./redux/store"
+import { persistor, store } from "./redux/store";
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <Background />
-        <Header />
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <Background />
+          <Header />
+          <App />
+        </PersistGate >
       </Provider >
     </BrowserRouter>
   </React.StrictMode>
