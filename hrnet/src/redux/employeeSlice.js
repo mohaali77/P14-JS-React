ba57c4be28f23d14894de752c0b06644119b797a
mohaali77 <mzr.mohand@gmail.com>
2023-12-24 11:16:04 +0100

    Update employeeSlice.js

--- hrnet/src/redux/employeeSlice.js
+++ hrnet/src/redux/employeeSlice.js
@@ -2,14 +2,16 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export const employeeSlice = createSlice({
     name: "employee",
-    initialState: [],
+    initialState: {
+        employee: [],
+    },
     reducers: {
         create: (state, action) => {
-            state.push(action.payload);
-        }
-    }
-})
+            state.employee.push(action.payload);
+        },
+    },
+});
 
 export const { create } = employeeSlice.actions;
-export const selectEmployee = (state) => state.employee
+export const selectEmployee = (state) => state.employee.employee;
 export default employeeSlice.reducer;
