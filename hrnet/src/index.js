46767e896ba3cabfe54a8109a5806e4d80946277
mohaali77 <mzr.mohand@gmail.com>
2023-12-24 09:50:14 +0100

    Update index.js

--- hrnet/src/index.js
+++ hrnet/src/index.js
@@ -5,14 +5,18 @@ import App from './App'
 import './App.css'
 import Header from './components/header/header';
 import Background from './components/background/background';
+import { Provider } from 'react-redux';
+import store from "./redux/store"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Background />
-      <Header />
-      <App />
+      <Provider store={store}>
+        <Background />
+        <Header />
+        <App />
+      </Provider >
     </BrowserRouter>
   </React.StrictMode>
 );
