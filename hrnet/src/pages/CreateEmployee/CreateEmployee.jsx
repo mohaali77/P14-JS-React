e15342bdce4ca9a27de816e65340ceec800f9381
mohaali77 <mzr.mohand@gmail.com>
2023-12-25 19:58:13 +0100

    Update CreateEmployee.jsx

--- hrnet/src/pages/CreateEmployee/CreateEmployee.jsx
+++ hrnet/src/pages/CreateEmployee/CreateEmployee.jsx
@@ -1,11 +1,14 @@
 import { Link } from "react-router-dom";
 import './style/CreateEmployee.css'
 import { Form } from "../../components/form/form";
+import { ModalDialog } from "../../components/modal-dialog/modal-dialog";
+
 
 export function CreateEmployee() {
     return <>
         <section className="container">
             <Form />
+            <ModalDialog />
             <Link to='/employee-list'>View Current Employees</Link>
         </section>
     </>
