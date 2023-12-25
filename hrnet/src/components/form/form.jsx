ca4854f8f1e1a947bd0213561a11fab89afbe331
mohaali77 <mzr.mohand@gmail.com>
2023-12-25 09:09:00 +0100

    Update form.jsx

--- hrnet/src/components/form/form.jsx
+++ hrnet/src/components/form/form.jsx
@@ -30,64 +30,65 @@ export function Form() {
     const errMsgDepartment = useRef(false);
 
 
-    const [employeeArray, setEmployeeArray] = useState(
-        localStorage.length <= 0 ? [] : JSON.parse(localStorage.getItem('employee')));
+    const [employeeArray, setEmployeeArray] = useState(localStorage.length <= 0 || localStorage.employee === 'null' ? [] : JSON.parse(localStorage.getItem('employee')));
     const [dataStates, setDataStates] = useState(statesArray);
     const [dataDepartments, setDataDepartments] = useState(departmentsArray);
-    const [isFormValid, setIsFormValid] = useState(null)
+    const [formData, setformData] = useState({
+        firstName: '',
+        lastName: '',
+        dateBirth: '',
+        dateState: '',
+        street: '',
+        city: '',
+        zipCode: '',
+        state: '',
+        department: ''
+    });
 
     const handleInputChange = () => { }
 
     const handleSubmit = (e) => {
         e.preventDefault()
 
-        function showErrorMsg() {
+        let isFormValid = true
 
-            const regexFirstName = /^[a-zA-ZÀ-ÖØ-öø-ÿ\s']{2,}$/;
-            const regexLastName = /^[a-zA-ZÀ-ÖØ-öø-ÿ\s']{2,}$/;
-            const regexCity = /^[a-zA-ZÀ-ÖØ-öø-ÿ\s'-]{2,}$/;
-            const regexStreet = /^[a-zA-Z0-9\s',.-]{2,}$/;
-            const regexZipcode = /^\d{5}(?:-\d{4})?$/;
+        const regexFirstName = /^[a-zA-ZÀ-ÖØ-öø-ÿ\s']{2,}$/;
+        const regexLastName = /^[a-zA-ZÀ-ÖØ-öø-ÿ\s']{2,}$/;
+        const regexCity = /^[a-zA-ZÀ-ÖØ-öø-ÿ\s'-]{2,}$/;
+        const regexStreet = /^[a-zA-Z0-9\s',.-]{2,}$/;
+        const regexZipcode = /^\d{5}(?:-\d{4})?$/;
 
+        if (inputFirstName.current.value === '' || regexFirstName.test(inputFirstName.current.value) === false) { errMsgFirst.current = true; isFormValid = false } else { errMsgFirst.current = false };
+        if (inputLastName.current.value === '' || regexLastName.test(inputLastName.current.value) === false) { errMsgLast.current = true; isFormValid = false } else { errMsgLast.current = false }
+        if (inputDateBirth.current.value === '' || inputDateBirth.current.value > inputDateStart.current.value) { errMsgBirth.current = true; isFormValid = false } else { errMsgBirth.current = false }
+        if (inputDateStart.current.value === '' || inputDateBirth.current.value > inputDateStart.current.value) { errMsgStart.current = true; isFormValid = false } else { errMsgStart.current = false }
+        if (inputStreet.current.value === '' || regexStreet.test(inputStreet.current.value) === false) { errMsgStreet.current = true; isFormValid = false } else { errMsgStreet.current = false }
+        if (inputCity.current.value === '' || regexCity.test(inputCity.current.value) === false) { errMsgCity.current = true; isFormValid = false } else { errMsgCity.current = false }
+        if (inputState.current.value === '') { errMsgState.current = true; isFormValid = false } else { errMsgState.current = false }
+        if (inputZipCode.current.value === '' || regexZipcode.test(inputZipCode.current.value) === false) { errMsgZipCode.current = true; isFormValid = false } else { errMsgZipCode.current = false }
+        if (inputDepartment.current.value === '') { errMsgDepartment.current = true; isFormValid = false } else { errMsgDepartment.current = false }
 
-            inputFirstName.current.value === '' || regexFirstName.test(inputFirstName.current.value) === false ? errMsgFirst.current = true : errMsgFirst.current = false;
-            inputLastName.current.value === '' || regexLastName.test(inputLastName.current.value) === false ? errMsgLast.current = true : errMsgLast.current = false
-            inputDateBirth.current.value === '' || inputDateBirth.current.value > inputDateStart.current.value ? errMsgBirth.current = true : errMsgBirth.current = false
-            inputDateStart.current.value === '' || inputDateBirth.current.value > inputDateStart.current.value ? errMsgStart.current = true : errMsgStart.current = false
-            inputStreet.current.value === '' || regexStreet.test(inputStreet.current.value) === false ? errMsgStreet.current = true : errMsgStreet.current = false
-            inputCity.current.value === '' || regexCity.test(inputCity.current.value) === false ? errMsgCity.current = true : errMsgCity.current = false
-            inputState.current.value === '' ? errMsgState.current = true : errMsgState.current = false
-            inputZipCode.current.value === '' || regexZipcode.test(inputZipCode.current.value) === false ? errMsgZipCode.current = true : errMsgZipCode.current = false
-            inputDepartment.current.value === '' ? errMsgDepartment.current = true : errMsgDepartment.current = false
-
-            errMsgFirst.current || errMsgLast.current || errMsgBirth.current || errMsgStart.current || errMsgStreet.current || errMsgCity.current || errMsgState.current || errMsgZipCode ? setIsFormValid(false) : setIsFormValid(true)
-
-            console.log('errorMsgFirst ' + errMsgFirst.current);
-            console.log('isFormValid ' + isFormValid);
-            //ajouter années minimum de différence entre date de commencement et date de naissance ?
-        }
-
-        showErrorMsg()
-
-        if (isFormValid) {
-            const newEmployee = {
-                firstname: inputFirstName.current.value,
-                lastname: inputLastName.current.value,
-                dateBirth: inputDateBirth.current.value,
-                dateStart: inputDateStart.current.value,
-                street: inputStreet.current.value,
-                city: inputCity.current.value,
-                state: inputState.current.value,
-                zipCode: inputZipCode.current.value,
-                department: inputDepartment.current.value,
-            }
-
-            setEmployeeArray((oldEmployee) => [...oldEmployee, (newEmployee)])
-        }
-
+        return isFormValid
     }
 
+
+
     useEffect(() => {
+
+        /*if (handleSubmit()) {
+        const newEmployee = {
+            firstname: inputFirstName.current.value,
+            lastname: inputLastName.current.value,
+            dateBirth: inputDateBirth.current.value,
+            dateStart: inputDateStart.current.value,
+            street: inputStreet.current.value,
+            city: inputCity.current.value,
+            state: inputState.current.value,
+            zipCode: inputZipCode.current.value,
+            department: inputDepartment.current.value,
+        }
+        setEmployeeArray((oldEmployee) => [...oldEmployee, (newEmployee)])
+    }*/
 
         if (localStorage.length <= 0) {
             localStorage.setItem('employee', [])
@@ -106,18 +107,18 @@ export function Form() {
                 </legend>
 
                 <label htmlFor="first-name">First Name</label>
-                <input required ref={inputFirstName} type="text" id="first-name" />
+                <input required ref={inputFirstName} type="text" id="first-name" value={formData.firstName} />
                 {errMsgFirst.current ? <p className='errorMsg'>Invalid Firstname</p> : null}
 
 
                 <label htmlFor="last-name">Last Name</label>
-                <input required ref={inputLastName} type="text" id="last-name" />
+                <input required ref={inputLastName} type="text" id="last-name" value={formData.lastName} />
                 {errMsgLast.current ? <p className='errorMsg'>Invalid Lastname</p> : null}
 
-                <DatePicker inputRef={inputDateBirth} onInputChange={handleInputChange} labelText='Date of Birth' id='date-of-birth' />
+                <DatePicker inputRef={inputDateBirth} onInputChange={handleInputChange} labelText='Date of Birth' id='date-of-birth' value={formData.birthDate} />
                 {errMsgBirth.current ? <p className='errorMsg'>Invalid Date of Birth</p> : null}
 
-                <DatePicker inputRef={inputDateStart} onInputChange={handleInputChange} labelText='Start Date' id='start-date' />
+                <DatePicker inputRef={inputDateStart} onInputChange={handleInputChange} labelText='Start Date' id='start-date' value={formData.startDate} />
                 {errMsgStart.current ? <p className='errorMsg'>Invalid Start Date</p> : null}
             </fieldset>
 
@@ -126,22 +127,22 @@ export function Form() {
                 <legend>Address</legend>
 
                 <label htmlFor="street">Street</label>
-                <input required ref={inputStreet} id="street" type="text" />
+                <input required ref={inputStreet} id="street" type="text" value={formData.street} />
                 {errMsgStreet.current ? <p className='errorMsg'>Invalid Street</p> : null}
 
                 <label htmlFor="city">City</label>
-                <input required ref={inputCity} id="city" type="text" />
+                <input required ref={inputCity} id="city" type="text" value={formData.city} />
                 {errMsgCity.current ? <p className='errorMsg'>Invalid City</p> : null}
 
-                <Menu inputRef={inputState} onInputChange={handleInputChange} data={dataStates} text='State' className='label-state' htmlFor='state' name='state' id="state" />
+                <Menu inputRef={inputState} onInputChange={handleInputChange} data={dataStates} text='State' className='label-state' htmlFor='state' name='state' id="state" value={formData.state} />
                 {errMsgState.current ? <p className='errorMsg'>Select a state</p> : null}
 
                 <label htmlFor="zip-code">Zip Code</label>
-                <input required ref={inputZipCode} id="zip-code" type="number" />
+                <input required ref={inputZipCode} id="zip-code" type="number" value={formData.firstName} />
                 {errMsgZipCode.current ? <p className='errorMsg'>Invalid Zip Code</p> : null}
 
             </fieldset>
-            <Menu inputRef={inputDepartment} onInputChange={handleInputChange} data={dataDepartments} text='Department' className='label-department' htmlFor='department' name='department' id="department" />
+            <Menu inputRef={inputDepartment} onInputChange={handleInputChange} data={dataDepartments} text='Department' className='label-department' htmlFor='department' name='department' id="department" value={formData.department} />
             {errMsgDepartment.current ? <p className='errorMsg'>Select a state</p> : null}
 
             <button onClick={handleSubmit} >Add employee</button>
