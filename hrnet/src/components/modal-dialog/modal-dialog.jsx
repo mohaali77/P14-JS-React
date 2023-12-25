871b77962497c85b6552ccdcd16d87cc0c36b844
mohaali77 <mzr.mohand@gmail.com>
2023-12-25 19:58:01 +0100

    Update modal-dialog.jsx

--- hrnet/src/components/modal-dialog/modal-dialog.jsx
+++ hrnet/src/components/modal-dialog/modal-dialog.jsx
@@ -1,8 +1,11 @@
 import './style/modal-dialog.css'
 
-
 export function ModalDialog() {
     return <>
-
+        <div className='modal-container'>
+            <div className='modal'>
+                <p>EMPLOYEE CREATE</p>
+            </div>
+        </div>
     </>
 }
