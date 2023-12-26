e9dcd4fcb2a46bd432726d3e26714e0b2e9ffc12
mohaali77 <mzr.mohand@gmail.com>
2023-12-26 21:15:12 +0100

    Update menu.jsx

--- hrnet/src/components/menu/menu.jsx
+++ hrnet/src/components/menu/menu.jsx
@@ -1,16 +1,41 @@
+import { useEffect, useRef, useState } from 'react'
 import './style/menu.css'
 
 
-export default function Menu({ className, name, id, forName, text, data, inputRef, onChange }) {
+export default function Menu({ className, name, text, data, inputRef, onChange }) {
+
+    const listRef = useRef()
+
+    const chooseList = (e) => {
+        console.log(e.target.textContent);
+        inputRef.current.value = e.target.textContent;
+        onChange(name, e.target.textContent);
+    }
+
+    useEffect(() => {
+
+        inputRef.current.addEventListener('click', (e) => {
+            if (listRef.current.classList.contains('mask')) {
+                listRef.current.classList.toggle('mask')
+            }
+        })
+
+        document.addEventListener('click', (e) => {
+            if (e.target !== inputRef.current && !listRef.current.classList.contains('mask')) {
+                listRef.current.classList.toggle('mask')
+            }
+        })
+
+    }, [])
 
     return <>
-        <label className={className} htmlFor={forName}>{text}</label>
-        <input type='text' required ref={inputRef} onChange={onChange} name={name} id={id} />
+        <label className={className} htmlFor={name}>{text}</label>
+        <input type='text' required ref={inputRef} onChange={onChange} name={name} id={name} />
         {//<li value="">Choose a {id}</li>
         }
-        <ul className={"list-" + name}  >
+        <ul ref={listRef} className={"mask list-" + name}  >
             {data.map((data) => (
-                <li key={data.abbreviation} value={data.name}>
+                <li onClick={chooseList} key={data.abbreviation} value={data.name}>
                     {data.name}
                 </li>
             ))}
