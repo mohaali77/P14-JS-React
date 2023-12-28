014c0e06f9cbd1b105cedb69f307208fe5ecbd9c
mohaali77 <mzr.mohand@gmail.com>
2023-12-28 17:55:09 +0100

    Update index.js

--- hrnet/src/index.js
+++ hrnet/src/index.js
@@ -10,8 +10,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      {/*<Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>*/}
       <Background />
       <Header />
       <App />
