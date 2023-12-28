29f18fce464ca36000d1dea9201b00ee201df1d5
mohaali77 <mzr.mohand@gmail.com>
2023-12-28 17:54:28 +0100

    Update index.js

--- hrnet/src/index.js
+++ hrnet/src/index.js
@@ -5,7 +5,6 @@ import App from './App'
 import './App.css'
 import Header from './components/header/header';
 import Background from './components/background/background';
-//import { persistor, store } from "./redux/store";
 //import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
