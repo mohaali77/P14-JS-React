89dc4c7817d4df21879b40637eecc10c2f4bb06e
mohaali77 <mzr.mohand@gmail.com>
2023-12-29 18:28:07 +0100

    Update EmployeeList.jsx

--- hrnet/src/pages/EmployeeList/EmployeeList.jsx
+++ hrnet/src/pages/EmployeeList/EmployeeList.jsx
@@ -1,10 +1,65 @@
 import { Link } from "react-router-dom";
 import './style/EmployeeList.css'
+import { useState } from 'react';
+import DataTable from 'react-data-table-component'
 
 export function EmployeeList() {
 
-    return <>
-        <h2>Current Employees</h2>
-        <Link to="/">Home</Link>
-    </>
+    const [data, setData] = useState(JSON.parse(localStorage.getItem('employee')))
+
+    const columns = [
+        {
+            name: 'First Name',
+            selector: row => row.firstName,
+            sortable: true
+        },
+        {
+            name: 'Last Name', selector: row => row.lastName,
+            sortable: true
+        },
+        {
+            name: 'Date of Birth', selector: row => row.dateBirth,
+            sortable: true
+        },
+        {
+            name: 'Start Date', selector: row => row.dateStart,
+            sortable: true
+        },
+        {
+            name: 'Street', selector: row => row.street,
+            sortable: true
+        },
+        {
+            name: 'City', selector: row => row.city,
+            sortable: true
+        },
+        {
+            name: 'State', selector: row => row.state,
+            sortable: true
+        },
+        {
+            name: 'Zip Code', selector: row =>
+                row.zipCode,
+            sortable: true,
+
+        },
+        {
+            name: 'Department', selector: row => row.department,
+            sortable: true
+        },
+    ];
+
+    return (
+        <>
+            <h2>Current Employees</h2>
+            <DataTable
+                columns={columns}
+                data={data}
+                fixedHeader
+                pagination
+                s />
+            <Link to="/">Home</Link>
+        </>
+    );
+
 }
