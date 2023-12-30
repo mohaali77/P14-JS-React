4b1989dcda85c790bc21b885a5bc1aedcb997c92
mohaali77 <mzr.mohand@gmail.com>
2023-12-30 22:52:41 +0100

    Update table.jsx

--- hrnet/src/components/table/table.jsx
+++ hrnet/src/components/table/table.jsx
@@ -6,6 +6,37 @@ import mockData from '../../data/mockData.json'
 
 export const Table = () => {
 
+    const [data, setData] = useState(mockData)
+    const columns = [
+        {
+            Header: 'First Name',
+            accessor: 'firstName'
+        }, {
+            Header: 'Last Name',
+            accessor: 'lastName'
+        }, {
+            Header: 'Date of Birth',
+            accessor: 'dateBirth'
+        }, {
+            Header: 'Start Date',
+            accessor: 'dateStart'
+        }, {
+            Header: 'Street',
+            accessor: 'street'
+        }, {
+            Header: 'City',
+            accessor: 'city'
+        }, {
+            Header: 'State',
+            accessor: 'state'
+        }, {
+            Header: 'Zip Code',
+            accessor: 'zipCode'
+        }, {
+            Header: 'Department',
+            accessor: 'department'
+        },
+    ]
 
     return <>
     </>
