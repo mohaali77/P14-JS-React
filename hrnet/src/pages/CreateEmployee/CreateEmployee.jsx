ffb5eda1d2fafb3b99222b27166eac62fa3f0123
mohaali77 <mzr.mohand@gmail.com>
2024-01-12 22:30:25 +0100

    Update CreateEmployee.jsx

--- hrnet/src/pages/CreateEmployee/CreateEmployee.jsx
+++ hrnet/src/pages/CreateEmployee/CreateEmployee.jsx
@@ -1,8 +1,6 @@
 import { Link } from "react-router-dom";
 import './style/CreateEmployee.css'
 import Form from "../../components/form/form";
-//import loadable from '@loadable/component';
-//const Form = loadable(() => import('../../components/form/form'));
 
 export function CreateEmployee() {
     return <>
