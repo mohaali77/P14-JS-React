ac4ff3da823d3ad49ba9494835d686250d4c9459
mohaali77 <mzr.mohand@gmail.com>
2024-02-11 21:27:47 +0100

    Update date-picker.jsx

--- hrnet/src/components/date-picker/date-picker.jsx
+++ hrnet/src/components/date-picker/date-picker.jsx
@@ -1,12 +1,12 @@
 import './style/date-picker.css'
 
 //fonction qui gère le composant DatePicker
-export default function DatePicker({ id, labelText, inputRef, onChange, name }) {
+export default function DatePicker({ id, labelText, onChange, name }) {
 
     return <>
         <label id={id} htmlFor={id}>
             {labelText}
         </label>
-        <input name={name} ref={inputRef} onChange={onChange} type="date" className={id} placeholder="A" />
+        <input name={name} onChange={onChange} type="date" className={id} placeholder="A" />
     </>
 }
