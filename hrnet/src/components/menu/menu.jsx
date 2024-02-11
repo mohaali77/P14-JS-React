b9039547c6598b894320d1a2d439db94dd300c46
mohaali77 <mzr.mohand@gmail.com>
2024-02-11 21:27:27 +0100

    Update menu.jsx

--- hrnet/src/components/menu/menu.jsx
+++ hrnet/src/components/menu/menu.jsx
@@ -2,13 +2,13 @@ import { useRef } from 'react'
 import './style/menu.css'
 
 //fonction qui gère le composant Menu
-export default function Menu({ errorMsg, name, text, data, inputRef, onChange }) {
+export default function Menu({ errorMsg, name, text, data, onChange }) {
 
     const errorRef = useRef()
 
     return <>
         <label className={name} htmlFor={name}>{text}</label>
-        <select required ref={inputRef} onChange={onChange} name={name} id={name}>
+        <select required onChange={onChange} name={name} id={name}>
             <option value="">Choose a {name}</option>
             {data.map((data) => (
                 <option key={data.abbreviation} value={data.name}>
